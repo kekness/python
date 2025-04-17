a407756512c80f323e2c1f9b96decaf1991af179
Piotr Benkowski <152896020+kekness@users.noreply.github.com>
2025-04-17 14:33:57 +0200

    obsługiwanie wielu list

--- toDoist/main.py
+++ toDoist/main.py
@@ -3,20 +3,23 @@ from TaskManager import TaskManager
 from Task import Task
 import argparse
 
-manager =TaskManager()
-manager.load_from_file()
-
 parser = argparse.ArgumentParser(description="Welcome to ToDoist")
 
 parser.add_argument("--add", nargs="+", metavar="TITLE", help="Add new task")
-parser.add_argument("--desc", metavar="DESCRIPTION", help = "Add description")
-parser.add_argument("--due", metavar="YYYY-MM-DD_HH:MM", help ="Set due date")
-parser.add_argument("--priority",metavar="INT", help= "set priority")
-parser.add_argument("--list", action="store_true",help="Show all tasks")
-parser.add_argument("--complete", metavar="NUMBER",type=int,help="Finish task")
+parser.add_argument("--desc", metavar="DESCRIPTION", help="Add description")
+parser.add_argument("--due", metavar="YYYY-MM-DD_HH:MM", help="Set due date")
+parser.add_argument("--priority", metavar="INT", help="Set priority")
+parser.add_argument("--list", action="store_true", help="Show all tasks")
+parser.add_argument("--complete", metavar="NUMBER", type=int, help="Finish task")
 parser.add_argument("--filter", metavar="BY", choices=["priority", "due", "created"], help="Sort tasks by: priority, due, created")
+parser.add_argument("--listname", metavar="NAME", default="tasks", help="Choose task list (default: 'tasks')")
 
 args = parser.parse_args()
+filename = f"{args.listname}.json"
+
+manager = TaskManager()
+manager.load_from_file(filename)
+
 
 if args.add:
     title = " ".join(args.add)
@@ -29,8 +32,8 @@ if args.add:
         except ValueError:
             print("Priority must be an integer.")
             exit()
-    due_date = None
 
+    due_date = None
     if args.due:
         try:
             due_date = datetime.strptime(args.due, "%Y-%m-%d_%H:%M")
@@ -38,19 +41,21 @@ if args.add:
             print("Wrong date format. Use YYYY-MM-DD_HH:MM")
             exit()
 
-    new_task = Task(title, description, due_date,completed,priority)
+    new_task = Task(title, description, due_date, completed, priority)
     manager.add_task(new_task)
-    manager.save_to_file()
+    manager.save_to_file(filename)
     print(f"Task submitted: {title}")
+
 
 elif args.complete:
     index = args.complete - 1
     if 0 <= index < len(manager.tasks):
         manager.tasks[index].complete()
-        manager.save_to_file()
+        manager.save_to_file(filename)
         print(f"Task {args.complete} marked as finished.")
     else:
         print(f"No such task was found")
+
 
 elif args.list:
     indexed_tasks = list(enumerate(manager.tasks))  # (index, task)
